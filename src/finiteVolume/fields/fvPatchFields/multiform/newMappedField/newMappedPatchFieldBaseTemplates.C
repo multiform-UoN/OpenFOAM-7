df53a68b314b8a3577784a50ff0542219375e98f
fmuni <federico.municchi@gmail.com>
2020-05-01 15:15:48 +0100

    Added more functions to newMapped

--- src/finiteVolume/fields/fvPatchFields/multiform/newMappedField/newMappedPatchFieldBaseTemplates.C
+++ src/finiteVolume/fields/fvPatchFields/multiform/newMappedField/newMappedPatchFieldBaseTemplates.C
@@ -124,6 +124,127 @@ newMappedPatchFieldBase<Type>::mappedVolField(word& name) const
 template<class Type>
 template<class T>
 tmp<Field<T>>
+newMappedPatchFieldBase<Type>::mappedGradField(word& name) const
+{
+    typedef GeometricField<T, fvPatchField, volMesh> fieldType;
+
+    // Since we're inside initEvaluate/evaluate there might be processor
+    // comms underway. Change the tag we use.
+    int oldTag = UPstream::msgType();
+    UPstream::msgType() = oldTag + 1;
+
+    //const fvMesh& thisMesh = patchField_.patch().boundaryMesh().mesh();
+    const fvMesh& nbrMesh = refCast<const fvMesh>(mapper_.sampleMesh());
+
+    // Result of obtaining remote values
+    tmp<Field<Type>> tnewValues(new Field<Type>(0));
+    Field<Type>& newValues = tnewValues.ref();
+
+    if (
+      (mapper_.mode() == mappedPatchBase::NEARESTPATCHFACE)
+      ||
+      (mapper_.mode() == mappedPatchBase::NEARESTPATCHFACEAMI)
+      )
+        {
+            const label nbrPatchID =
+                nbrMesh.boundaryMesh().findPatchID(mapper_.samplePatch());
+
+            if (nbrPatchID < 0)
+            {
+                FatalErrorInFunction
+                 << "Unable to find sample patch " << mapper_.samplePatch()
+                 << " in region " << mapper_.sampleRegion()
+                 << " for patch " << patchField_.patch().name() << nl
+                 << abort(FatalError);
+            }
+
+            const fieldType& nbrField = sampleVolField(name);
+
+            newValues =
+              nbrField.boundaryField()[nbrPatchID].patch().deltaCoeffs() *
+              (
+              nbrField.boundaryField()[nbrPatchID] -
+              nbrField.boundaryField()[nbrPatchID].patchInternalField()
+              );
+            mapper_.distribute(newValues);
+
+        }
+        else
+        {
+            FatalErrorInFunction
+             << "Use nearestPatchFace only instead of " << mapper_.mode()
+             << nl << abort(FatalError);
+        }
+
+
+    // Restore tag
+    UPstream::msgType() = oldTag;
+
+    return tnewValues;
+}
+
+template<class Type>
+template<class T>
+tmp<Field<T>>
+newMappedPatchFieldBase<Type>::mappedInternalField(word& name) const
+{
+    typedef GeometricField<T, fvPatchField, volMesh> fieldType;
+
+    // Since we're inside initEvaluate/evaluate there might be processor
+    // comms underway. Change the tag we use.
+    int oldTag = UPstream::msgType();
+    UPstream::msgType() = oldTag + 1;
+
+    //const fvMesh& thisMesh = patchField_.patch().boundaryMesh().mesh();
+    const fvMesh& nbrMesh = refCast<const fvMesh>(mapper_.sampleMesh());
+
+    // Result of obtaining remote values
+    tmp<Field<Type>> tnewValues(new Field<Type>(0));
+    Field<Type>& newValues = tnewValues.ref();
+
+    if (
+      (mapper_.mode() == mappedPatchBase::NEARESTPATCHFACE)
+      ||
+      (mapper_.mode() == mappedPatchBase::NEARESTPATCHFACEAMI)
+      )
+        {
+            const label nbrPatchID =
+                nbrMesh.boundaryMesh().findPatchID(mapper_.samplePatch());
+
+            if (nbrPatchID < 0)
+            {
+                FatalErrorInFunction
+                 << "Unable to find sample patch " << mapper_.samplePatch()
+                 << " in region " << mapper_.sampleRegion()
+                 << " for patch " << patchField_.patch().name() << nl
+                 << abort(FatalError);
+            }
+
+            const fieldType& nbrField = sampleVolField(name);
+
+            newValues =
+              nbrField.boundaryField()[nbrPatchID].patchInternalField();
+              
+            mapper_.distribute(newValues);
+
+        }
+        else
+        {
+            FatalErrorInFunction
+             << "Use nearestPatchFace only instead of " << mapper_.mode()
+             << nl << abort(FatalError);
+        }
+
+
+    // Restore tag
+    UPstream::msgType() = oldTag;
+
+    return tnewValues;
+}
+
+template<class Type>
+template<class T>
+tmp<Field<T>>
 newMappedPatchFieldBase<Type>::mappedSurField(word& name) const
 {
     typedef GeometricField<T, fvsPatchField, surfaceMesh> fieldType;
